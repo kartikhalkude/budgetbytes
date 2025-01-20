0ce8e3d9e21ab22a0c318595766a7174d752a22b
kartikhalkude <kartikhalkude@gmail.com>
2025-01-20 18:49:52 +0530

    Add files via upload

---
+++ userdata.js
@@ -0,0 +1,7 @@
+const users = [
+    { username: "ayush", password: "123" },
+    { username: "kartik", password: "123" },
+    { username: "anni", password: "123" },
+    { username: "appa", password: "456" },
+    { username: "niggesh", password: "911" },
+  ];
